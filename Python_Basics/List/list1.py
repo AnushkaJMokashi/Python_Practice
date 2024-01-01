3a9e5beb91277758c597ad4478281bd2e0acf251
Anushka Mokashi <anushkamokashi@gmail.com>
2024-01-01 23:37:34 +0530

    Python Revision1

--- Python_Basics/List/list1.py
+++ Python_Basics/List/list1.py
@@ -2,5 +2,21 @@ l = [3,5,6]
 print(l)
 print(type(l))
 ## list are ordered collection of data items
-
 print(l[0])
+## negative index are allowed
+print(l[-3]) 
+## pos index = len-3
+print(l[5-3])
+print(l[len(l)-3])
+
+print('-------------------------------------')
+## Any datatype can be there
+l = [1,"A",8.9,True]
+print(l)
+
+print('-------------------------------------')
+if 7 in l:
+    print("yes")
+else:
+    print("No")
+    
