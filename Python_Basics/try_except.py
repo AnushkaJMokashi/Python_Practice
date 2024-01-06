28d71d71fe521dae3d95d67a5091cec7557ecf45
Anushka Mokashi <anushkamokashi@gmail.com>
2024-01-06 23:52:38 +0530

    Python Revision6

---
+++ Python_Basics/try_except.py
@@ -0,0 +1,8 @@
+try:
+    x = 10 / 0  # This will raise a ZeroDivisionError
+except ZeroDivisionError as e:
+    print(f"Error: {e}")
+else:
+    print("No error occurred.")
+finally:
+    print("This code always runs.")
