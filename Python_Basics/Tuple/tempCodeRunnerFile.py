e7283c757e7837dedbbbda54fb52394886232bc2
Anushka Mokashi <anushkamokashi@gmail.com>
2024-01-07 22:51:57 +0530

    Python Revision7

--- Python_Basics/Tuple/tempCodeRunnerFile.py
+++ Python_Basics/Tuple/tempCodeRunnerFile.py
@@ -1,11 +1,8 @@
-## Occurrences of element
-tup1 = (0,1,1,3,1,1,2,3,4,5,6,7)
-res = tup1.count(1)
-print("Count of 1 is: ",res)
 
-##index
-ind = tup1.index(1)
-print("Index of 1 is: ",ind)
+tup = (1)
+print(type(tup),tup)
+tup1 = (1,)
+print(tup1)
 
-ind1 = tup1.index(3,4,8)
-print(ind1)
+tup1[0] = 90
+print(tup1)
---
+++ Python_Basics/Tuple/tuples.py
@@ -0,0 +1,13 @@
+## TUPLES
+
+# Tuples are ordered collection of data items.
+# They store multiple items in a single variable.
+# Tuple are immutable
+
+tup = (1)
+print(type(tup),tup)
+tup1 = (1,)
+print(tup1)
+
+tup1[0] = 90
+print(tup1)
